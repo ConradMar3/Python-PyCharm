765d1855c8def9b1532f9bcb4f3e2915f3df9c6e
ConradMare <conradmare890317@gmail.com>
2022-12-04 06:21:01 +0200

    2022/12/04 (am)
    Chapters  10 & 16

--- Chapter9/electric_car.py
+++ Chapter9/electric_car.py
@@ -1,11 +1,12 @@
-from car import *
 from battery import *
+from car import *
+
 
 class ElectricCar(Car):
-	"""Represent aspects of a car, specific to electric vehicles."""
+    """Represent aspects of a car, specific to electric vehicles."""
 
-	def __init__(self, make, model, year):
-		"""Initialize attributes of the parent class."""
-		super().__init__(make, model, year)
-		self.battery_size = 75
-		self.battery = Battery()
+    def __init__(self, make, model, year):
+        """Initialize attributes of the parent class."""
+        super().__init__(make, model, year)
+        self.battery_size = 75
+        self.battery = Battery()
