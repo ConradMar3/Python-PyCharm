5bdeaea59731240d1ac353cf1fb502e17aef676d
ConradMare <conradmare890317@gmail.com>
2022-12-05 09:51:57 +0200

    20222/12/05 (am)

--- Chap16/sitka_highs.py
+++ Chap16/sitka_highs.py
@@ -1,3 +1,5 @@
+import csv
+
 filename = 'data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
 	reader = csv.reader(f)
--- Chap16/world_map.py
+++ Chap16/world_map.py
@@ -4,7 +4,7 @@ from plotly import offline
 for eq_dict in all_eq_dicts:
 
 # Map the earthquakes.
-data = [Scattergeo(lon=lons, lat=lats)]
+data = [Scattergeo(lon=eq_dict, lat=eq_dict)]
 my_layout = Layout(title='Global Earthquakes')
 fig = {'data': data, 'layout': my_layout}
 offline.plot(fig, filename='global_earthquakes.html')
