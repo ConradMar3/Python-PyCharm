e9a5972358206b7b709b4fac716c81bf86328ad4
ConradMare <conradmare890317@gmail.com>
2022-12-06 13:01:08 +0200

    2022/12/06 (pm)
    Python 3 - IDE = 'PyCharcm'.

---
+++ Matplotlib_Advanced_Python/3D_plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+x1 = np.random.rand(100)
+y1 = np.random.rand(100)
+z1 = np.random.rand(100)
+ax.scatter(x1, y1, z1, s=20, color='red')
+
+plt.show()
---
+++ Rolling_Dice/die_visual.py
@@ -0,0 +1,26 @@
+# Create a D6.
+from Rolling_Dice.die import Die
+
+die = Die()
+
+# Roll, and store results in a list.
+results = []
+
+# for roll_num in range(100):
+result = die.roll()
+results.append(result)
+
+print(results)
+
+# Analyzing the results.
+for roll_num in range(1000):
+    result = die.roll()
+results.append(result)
+
+frequencies = []
+
+for value in range(1, die.num_sides + 1):
+    frequency = results.count(value)
+frequencies.append(frequency)
+
+print(frequencies)
